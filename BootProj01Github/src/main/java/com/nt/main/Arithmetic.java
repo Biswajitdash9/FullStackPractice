b0b52547a659102edf5c68d93bb40231a022470b
Biswajitdash9 <dashbiswajit751@gmail.com>
2025-08-17 15:38:39 +0530

    Dev1 add comment to arithmetic class

--- BootProj01Github/src/main/java/com/nt/main/Arithmetic.java
+++ BootProj01Github/src/main/java/com/nt/main/Arithmetic.java
@@ -4,8 +4,11 @@ public class Arithmetic {
 
 	public void sum()
 	{
+		//Numbers are going to participate in sum operation
 	double num1=25;
 	double num2= 35;
+	
+	
    double sum=num1+num2;
    
    System.out.println(sum);
